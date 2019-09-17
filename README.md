eed6649763b69af5f38b6efc5aa456004313ce7b
Robert Vaughan <robertjwvaughan@gmail.com>
2019-09-17 22:59:29 +0100

    Batch edits

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # misc
 A repo to contain misc files created during random hacking sessions
+
+## Notes
+
+git rev-parse HEAD
+
+git ls-remote https://username:password@github.com/robertjwvaughan/mis
+
+https://devblogs.microsoft.com/oldnewthing/20120731-00/?p=7003
+
+https://stackoverflow.com/questions/19878136/how-can-i-use-a-batch-file-to-write-to-a-text-file/19879594
--- batch/auto-build.bat
+++ batch/auto-build.bat
@@ -1,6 +1,22 @@
 @ECHO OFF
-for /f "tokens=5 delims= " %%a in ('netstat -aon ^| find ":8085" ^| find "LISTENING"') do taskkill /f /pid %%a
+rem kill firefox
+taskkill /IM firefox.exe
+
+rem fetch processes running on port 8080 and kill them
+for /f "tokens=5 delims= " %%a in (
+	'netstat -aon ^| find ":8085" ^| find "LISTENING"'
+) do (
+	taskkill /f /pid %%a
+	goto git-phase
+)
+
+:git-phase
+rem Go to the JAR build
 cd C:\Users\thewo\dev\gs-rest-service\initial
-git fetch --all
-git reset --hard origin/master
-mvn package && javaw -jar target/gs-rest-service-0.1.0.jar --server.port=8085 && start "" http://localhost:8085/greeting
+
+rem Git fetch and reset to master for no merging worries
+rem git fetch --all
+rem git reset --hard origin/master
+
+rem Build Maven and run the JAR as a background process [shall be task listed as javaw.exe]
+mvn package && javaw -jar target/gs-rest-service-0.1.0.jar --server.port=8085 && timeout /t 10 > NUL && start "" http://localhost:8085/greeting
